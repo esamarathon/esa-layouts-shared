7af21e2816fe496712a2f8861b806cd5d3067e77
zoton2 <zoton2@gmail.com>
2020-06-08 22:17:59 +0100

    Some more eslint/tsconfig tweaks

--- rabbitmq/extension/.eslintrc.js
+++ rabbitmq/extension/.eslintrc.js
@@ -1,44 +1,6 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
+  extends: ['../../.eslintrc.extension.js'],
   parserOptions: {
     project: 'tsconfig.json',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'airbnb-base',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-  ],
-  settings: {
-    'import/core-modules': ['nodecg/types/server'],
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-    'import/resolver': {
-      node: {
-        moduleDirectory: [
-          'node_modules',
-          '../..',
-          '..',
-          '.',
-        ],
-      },
-    },
-  },
-  rules: {
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-      jsx: 'never',
-      ts: 'never',
-      tsx: 'never',
-    }],
-    'import/no-extraneous-dependencies': ['error', {
-      packageDir: ['.', '../..'],
-    }],
-    'lines-between-class-members': 'off',
-    'max-len': ['error', { 'code': 100 }],
-    'class-methods-use-this': 'off',
   },
 };
