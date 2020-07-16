c821308ebc40ecabc58750601a65d8e3854b8a8c
zoton2 <zoton2@gmail.com>
2020-07-16 16:09:27 +0100

    Restream extension: store data, properly change streams

--- restream/extension/src/index.ts
+++ restream/extension/src/index.ts
@@ -17,6 +17,12 @@ function buildSchemaPath(schemaName: string) {
   return path.resolve(__dirname, '../../../schemas', `${encodeURIComponent(schemaName)}.json`);
 }
 
+interface RestreamInstance {
+  on(event: 'connected', listener: () => void): this;
+  on(event: 'disconnected', listener: () => void): this;
+  on(event: 'update', listener: (data: RestreamTypes.UpdateMsg) => void): this;
+}
+
 class RestreamInstance extends EventEmitter {
   private ws: WebSocket | undefined;
   private nodecg: NodeCG;
@@ -34,7 +40,8 @@ class RestreamInstance extends EventEmitter {
   async sendMsg(msg: RestreamTypes.AllSentMsg): Promise<RestreamTypes.ResponseMsg> {
     return new Promise((res) => {
       if (!this.ws || this.ws.readyState !== 1) {
-        throw new Error('WebSocket not connected');
+        // throw new Error('WebSocket not connected');
+        return;
       }
       const msgID = uuid();
       this.ws.send(JSON.stringify({ ...msg, ...{ msgID } }));
@@ -55,6 +62,7 @@ class RestreamInstance extends EventEmitter {
     this.ws = new WebSocket(`ws://${this.address}/?key=${this.key}`);
 
     this.ws.once('open', () => {
+      this.emit('connected');
       // log open
     });
 
@@ -66,13 +74,15 @@ class RestreamInstance extends EventEmitter {
       if (this.ws) {
         this.ws.removeAllListeners();
       }
+      // log close
+      this.emit('disconnected');
       setTimeout(() => this.connect(), 5 * 1000);
     });
 
     this.ws.on('message', (data) => {
       const msg: RestreamTypes.IncomingMsg = JSON.parse(data.toString());
       if (msg.type === 'Update') {
-        // store update msg
+        this.emit('update', msg);
       }
     });
   }
@@ -103,14 +113,12 @@ class RestreamInstance extends EventEmitter {
 
 class Restream {
   private nodecg: NodeCG;
-  private config: RestreamTypes.Config;
   private sc: SpeedcontrolUtil | undefined;
   private restreamData: Replicant<RestreamData>
   private instances: RestreamInstance[] = [];
 
   constructor(nodecg: NodeCG, sc: boolean, config: RestreamTypes.Config) {
     this.nodecg = nodecg;
-    this.config = config;
     if (sc) {
       this.sc = new SpeedcontrolUtil(nodecg);
     }
@@ -124,9 +132,24 @@ class Restream {
     if (config.enable) {
       const cfgArr = (Array.isArray(config.instances)) ? config.instances : [config.instances];
 
-      this.instances = cfgArr.map((cfg) => {
+      // Add defaults to the replicant if needed.
+      if (this.restreamData.value.length < cfgArr.length) {
+        const count = cfgArr.length - this.restreamData.value.length;
+        const defaultData: RestreamData[0] = {
+          connected: false,
+          overridden: false,
+          lowLatency: true,
+        };
+        this.restreamData.value.push(...Array(count).fill(defaultData));
+      }
+
+      this.instances = cfgArr.map((cfg, i) => {
         const restream = new RestreamInstance(nodecg, cfg.address, cfg.key);
-        // may need to add listeners here
+        restream.on('connected', () => { this.restreamData.value[i].connected = true; });
+        restream.on('disconnected', () => { this.restreamData.value[i].connected = false; });
+        restream.on('update', ({ lowLatency, channel, uuid: uuid_ }) => {
+          this.updateData(i, { lowLatency, channel, uuid: uuid_ });
+        });
         return restream;
       });
 
@@ -134,50 +157,88 @@ class Restream {
         // Start new stream when run changes but not on server (re)start.
         let init = false;
         this.sc.runDataActiveRun.on('change', async (newVal, oldVal) => {
-          if (init && newVal?.id !== oldVal?.id
-            && newVal && newVal.teams.length && newVal.teams[0].players.length
-            && newVal.teams[0].players[0].social.twitch) {
-            // start/change needed streams
+          if (init && newVal?.id !== oldVal?.id && newVal && newVal.teams.length) {
+            this.instances.forEach(async (instance, i) => {
+              const player = newVal.teams[i]?.players[0];
+              if (instance && player && player.social.twitch) {
+                const { lowLatency, channel, uuid: uuid_ } = await instance.startStream(
+                  player.social.twitch,
+                  this.restreamData.value[i]?.lowLatency,
+                );
+                this.updateData(i, {
+                  overridden: false,
+                  lowLatency,
+                  channel,
+                  uuid: uuid_,
+                });
+              }
+            });
           }
           init = true;
         });
       }
 
-      // eslint-disable-next-line max-len
-      this.nodecg.listenFor('rvtOverride', async (data: { index?: number; channel?: string; lowLatency?: boolean } = {}, cb) => {
+      this.nodecg.listenFor('restreamOverride', async (data: {
+        index?: number;
+        channel?: string;
+        lowLatency?: boolean;
+      } = {}, cb) => {
         const instance = this.instances[data.index || 0];
         const channel = data.channel || this.restreamData.value[data.index || 0]?.channel;
         if (instance && channel) {
-          await instance.startStream(
+          const { lowLatency, channel: channel_, uuid: uuid_ } = await instance.startStream(
             channel,
-            data.lowLatency ?? this.restreamData.value[data.index || 0]?.lowLatency ?? true,
+            data.lowLatency ?? this.restreamData.value[data.index || 0]?.lowLatency,
           );
+          this.updateData(data.index || 0, {
+            overridden: true,
+            lowLatency,
+            channel: channel_,
+            uuid: uuid_,
+          });
         }
         if (cb && !cb.handled) {
           cb();
         }
       });
 
-      this.nodecg.listenFor('rvtRestart', async (data: { index?: number } = {}, cb) => {
+      this.nodecg.listenFor('restreamRestart', async (data: { index?: number } = {}, cb) => {
         const instance = this.instances[data.index || 0];
         if (instance) {
-          await instance.restartStream();
+          const { lowLatency, channel, uuid: uuid_ } = await instance.restartStream();
+          this.updateData(data.index || 0, { lowLatency, channel, uuid: uuid_ });
         }
         if (cb && !cb.handled) {
           cb();
         }
       });
 
-      this.nodecg.listenFor('rvtStop', async (data: { index?: number } = {}, cb) => {
+      this.nodecg.listenFor('restreamStop', async (data: { index?: number } = {}, cb) => {
         const instance = this.instances[data.index || 0];
         if (instance) {
-          await instance.stopStream();
+          const { lowLatency, channel, uuid: uuid_ } = await instance.stopStream();
+          this.updateData(data.index || 0, { lowLatency, channel, uuid: uuid_ });
         }
         if (cb && !cb.handled) {
           cb();
         }
       });
     }
+  }
+
+  updateData(i: number, opts: {
+    channel?: string,
+    uuid?: string,
+    overridden?: boolean,
+    lowLatency?: boolean,
+  }): void {
+    this.restreamData.value[i] = {
+      connected: this.restreamData.value[i].connected,
+      overridden: opts.overridden ?? this.restreamData.value[i].overridden,
+      lowLatency: opts.lowLatency ?? this.restreamData.value[i].lowLatency,
+      channel: opts.channel,
+      uuid: opts.uuid || this.restreamData.value[i].uuid,
+    };
   }
 }
 
