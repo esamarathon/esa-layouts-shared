f1ca92e138108638f07ed9d0a3dbe96e55eba0e3
zoton2 <zoton2@gmail.com>
2021-03-10 16:24:28 +0000

    Add script to run child npm scripts, add/fix parent npm scripts

---
+++ cmd.js
@@ -0,0 +1,31 @@
+const globby = require('globby');
+const { dirname } = require('path');
+const concurrently = require('concurrently');
+const yargs = require('yargs');
+
+const args = yargs.options({
+  'c': {
+    alias: 'context',
+    type: 'string',
+    choices: [
+      'dashboard',
+      'extension',
+    ],
+    default: 'extension',
+  },
+  's': {
+    alias: 'script',
+    type: 'string',
+    choices: [
+      'autofix',
+      'build',
+      'watch',
+    ],
+    default: 'build',
+  }
+}).argv;
+
+const packages = globby.sync(`*/${args.c}/package.json`);
+const dirs = packages.map((pkg) => dirname(pkg));
+concurrently(dirs.map((d) => ({ command: `cd ${d} && npm run ${args.s}` })))
+  .catch((err) => { console.log(err); });
